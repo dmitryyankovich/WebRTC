48782abb1d97e9b56ec4de1e1d74e228ff0d46c8
unknown <dzmitry.yankovich@wsa-187.icx.local>
2015-01-10 16:33:55 +0300

    initial commit

---
+++ WebRTCTestApp/WebRTCTestApp/Controllers/WebRTCController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRTCTestApp.Controllers
+{
+    public class WebRTCController : Controller
+    {
+        public ActionResult Room()
+        {
+            return View();
+        }
+    }
+}
